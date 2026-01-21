766f00b7c032ad0ec9bf30d11ece904794b201df
Ngong kuot <ngongkuot6@gmail.com>
2026-01-21 15:08:25 +0300

    Delete src/data directory

--- src/data/reportStore.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file stores reports temporarily in memory.
- * Data will be lost when the server restarts.
- */
-
-// Array to store reports
-const reports = [];
-
-module.exports = reports;
