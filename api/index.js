734903cc6cabea09c4f8b0b513f107787fee0c9e
deng_malou <dengmalou74@gmail.com>
2026-01-21 17:59:06 +0300

    Setup Vercel serverless function handler for Express app

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const serverless = require("serverless-http");
+const app = require("../app");
+const connectDB = require("../config/db");
+
+connectDB();
+
+module.exports.handler = serverless(app);
