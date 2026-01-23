df04ea8530dfb75db3cb8e670c6f57d48ff8b0aa
deng_malou <dengmalou74@gmail.com>
2026-01-23 16:06:36 +0300

    Rename model folder to data and fix reportController casing

---
+++ src/data/report.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Report", reportSchema);
